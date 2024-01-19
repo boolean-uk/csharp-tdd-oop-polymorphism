1094411762ad2227995d0f61f04df92cb87bb813
Mahmoud Ali <mahmoodali2965.ma@gmail.com>
2024-01-19 13:30:00 +0100

    Mahmoud Ali

--- tdd-oop-polymorphism.CSharp.Main/Basket.cs
+++ tdd-oop-polymorphism.CSharp.Main/Basket.cs
@@ -8,68 +8,27 @@ namespace tdd_oop_polymorphism.CSharp.Main
 {
     public class Basket
     {
-        List<Game> games = new List<Game>();
-        List<Drink> drinks = new List<Drink>();
-        List<Book> books = new List<Book>();
+        List<Interface1> list = new List<Interface1>();
 
-        public void add(Game game)
+        public void add(Interface1 item)
         {
-            this.games.Add(game);
+            this.list.Add(item);
         }
-
-        public void add(Drink drink)
-        {
-            this.drinks.Add(drink);
-        }
-
-        public void add(Book book)
-        {
-            this.books.Add(book);
-        }
-
         public int getTotal()
         {
             int total = 0;
 
-            foreach (Game game in this.games)
+            foreach (Interface1 item in list)
             {
-                total += game.getPrice();
+                total += item.price;
             }
-
-            foreach (Drink drink in this.drinks)
-            {
-                total += drink.getPrice();
-            }
-
-            foreach (Book book in this.books)
-            {
-                total += book.getPrice();
-            }
-
             return total;
         }
-
         public bool isInBasket(String name)
         {
-            foreach (Game game in this.games)
+            foreach (Interface1 item in list)
             {
-                if (game.getName().Equals(name))
-                {
-                    return true;
-                }
-            }
-
-            foreach (Drink drink in this.drinks)
-            {
-                if (drink.getName().Equals(name))
-                {
-                    return true;
-                }
-            }
-
-            foreach (Book book in this.books)
-            {
-                if (book.getName().Equals(name))
+                if (item.Name.Equals(name))
                 {
                     return true;
                 }
