cfdad72cd4626821b31b476458868c7fb76853b9
Oneallane <oneallane2808@gmail.com>
2024-01-19 10:04:23 +0100

    C#EX3 Oneal Lane

--- tdd-oop-polymorphism.CSharp.Main/Basket.cs
+++ tdd-oop-polymorphism.CSharp.Main/Basket.cs
@@ -8,42 +8,22 @@ namespace tdd_oop_polymorphism.CSharp.Main
 {
     public class Basket
     {
-        List<Game> games = new List<Game>();
-        List<Drink> drinks = new List<Drink>();
-        List<Book> books = new List<Book>();
-
-        public void add(Game game)
+        List<IProduct> products = new List<IProduct>();
+       
+        public void add(IProduct item)
         {
-            this.games.Add(game);
+            this.products.Add(item);
         }
 
-        public void add(Drink drink)
-        {
-            this.drinks.Add(drink);
-        }
-
-        public void add(Book book)
-        {
-            this.books.Add(book);
-        }
+       
 
         public int getTotal()
         {
             int total = 0;
 
-            foreach (Game game in this.games)
+            foreach (IProduct product in this.products)
             {
-                total += game.getPrice();
-            }
-
-            foreach (Drink drink in this.drinks)
-            {
-                total += drink.getPrice();
-            }
-
-            foreach (Book book in this.books)
-            {
-                total += book.getPrice();
+                total += product.getPrice();
             }
 
             return total;
@@ -51,29 +31,14 @@ namespace tdd_oop_polymorphism.CSharp.Main
 
         public bool isInBasket(String name)
         {
-            foreach (Game game in this.games)
+            foreach (IProduct product in this.products)
             {
-                if (game.getName().Equals(name))
+                if (product.getName().Equals(name))
                 {
                     return true;
                 }
             }
 
-            foreach (Drink drink in this.drinks)
-            {
-                if (drink.getName().Equals(name))
-                {
-                    return true;
-                }
-            }
-
-            foreach (Book book in this.books)
-            {
-                if (book.getName().Equals(name))
-                {
-                    return true;
-                }
-            }
 
             return false;
         }
