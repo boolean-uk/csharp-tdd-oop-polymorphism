2fcb08680bdfefd019238ce98d7d128e58f120e3
Santhia <santhia.shanmukarasan@gmail.com>
2024-01-22 09:37:09 +0100

    Exercise completed:)

--- tdd-oop-polymorphism.CSharp.Main/Basket.cs
+++ tdd-oop-polymorphism.CSharp.Main/Basket.cs
@@ -2,19 +2,38 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace tdd_oop_polymorphism.CSharp.Main
 {
     public class Basket
     {
+        // New list to store products of any kind of type that implements IProduct
+
+        List<IProduct> products = new List<IProduct>();
+
+        /* Old code is no longer needed cause we just need a single list for all product types
+         * thorugh common interface IProduct.
+       
         List<Game> games = new List<Game>();
         List<Drink> drinks = new List<Drink>();
         List<Book> books = new List<Book>();
+        */
 
-        public void add(Game game)
+        //Updated method to accept any Iproduct
+        public void add(IProduct product)
         {
-            this.games.Add(game);
+            this.products.Add(product);
+        }
+
+        // The following methods are no longer needed
+
+        /*
+        public void add(IProduct product)
+        {
+            this.products.Add(product);
         }
 
         public void add(Drink drink)
@@ -26,11 +45,22 @@ namespace tdd_oop_polymorphism.CSharp.Main
         {
             this.books.Add(book);
         }
+        */
 
+        //Updated method to calculae the total price using only the IProduct interface
         public int getTotal()
         {
             int total = 0;
+            // Loop through the list of IProduct and add up their prices to calculate the total cos
+            foreach (IProduct product in this.products)
+            {
+                total += product.Price;
+            }
 
+            return total;
+
+            //The old code for separate loops for game, drink and books is no longer needed
+            /*
             foreach (Game game in this.games)
             {
                 total += game.getPrice();
@@ -47,10 +77,24 @@ namespace tdd_oop_polymorphism.CSharp.Main
             }
 
             return total;
+            */
+            
         }
-
-        public bool isInBasket(String name)
+        // Loop through the list of products and check if any has a name that matches the specified name
+        public bool isInBasket(string name)
         {
+            foreach (IProduct product in this.products)
+            {
+                if (product.Name.Equals(name))
+                {
+                    return true;
+                }
+            }
+            return false;
+
+
+            // The old code with separate loops for games, drinks, and books is no longer needed
+            /*
             foreach (Game game in this.games)
             {
                 if (game.getName().Equals(name))
@@ -74,8 +118,8 @@ namespace tdd_oop_polymorphism.CSharp.Main
                     return true;
                 }
             }
-
             return false;
+            */
         }
     }
 }
---
+++ tdd-oop-polymorphism.CSharp.Main/IProduct.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tdd_oop_polymorphism.CSharp.Main
+{
+    //IProduct interface declares properties for the name and price of a product.
+    public interface IProduct
+    {
+        string Name { get; set; }
+        int Price { get; set; }
+    }
+}
